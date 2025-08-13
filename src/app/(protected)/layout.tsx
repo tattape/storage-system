6bbc850d58e3e4f2eb7b62da0211e51dadc18829
Tattape Sonklin <tsc_com@hotmail.com>
2025-08-13 17:53:04 +0700

    fix: remove files

--- src/app/(protected)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ProtectedLayout from "@/storage/components/ProtectedLayout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ProtectedLayout>{children}</ProtectedLayout>;
-}
